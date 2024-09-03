6c86348dddbba68530644e71b574fcb082d065e8
kidwen <xuqiujia2020@outlook.com>
2024-09-03 11:08:46 +0800

    Site updated: 2024-09-03 11:08:45

---
+++ downloads/script/console.js
@@ -0,0 +1,26 @@
+function consoleName() {
+    console.clear();
+    console.log(
+        String.raw`%c
+        ██╗  ██╗██╗██████╗ ██╗    ██╗███████╗███╗   ██
+        ██║ ██╔╝██║██╔══██╗██║    ██║██╔════╝████╗  ██║
+        █████╔╝ ██║██║  ██║██║ █╗ ██║█████╗  ██╔██╗ ██║
+        ██╔═██╗ ██║██║  ██║██║███╗██║██╔══╝  ██║╚██╗██║
+        ██║  ██╗██║██████╔╝╚███╔███╔╝███████╗██║ ╚████║
+        ╚═╝  ╚═╝╚═╝╚═════╝  ╚══╝╚══╝ ╚══════╝╚═╝  ╚═══╝
+        `,
+        "color: #6156f4;"
+    );
+}
+consoleName();
+
+function headerBar() {
+    const body = document.getElementById('body-container');
+    if(body.clientWidth < 712) return;
+    const bar = document.createElement('div');
+    bar.classList.add('page-alert');
+    bar.innerText = `Say no to the act that Japan wees and poops its damn RADIOACTIVE water to our OCEAN! ☠️☠️☠️`;
+    body.insertBefore(bar, body.children[0]);
+}
+
+headerBar();
